09692de4c789d2867619a3444aec59d065dec3f9
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-11-02 18:16:43 +0100

    datepicker in spanish

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -2,8 +2,12 @@ import { useState } from 'react';
 import { addHours } from 'date-fns';
 
 import Modal from 'react-modal';
-import DatePicker from 'react-datepicker';
+import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import es from 'date-fns/locale/es';
+
+
+registerLocale('es', es);
 
 const customStyles = {
     content: {
@@ -68,6 +72,9 @@ export const CalendarModal = () => {
                     onChange={ (event) => onDateChanged(event, 'start')}
                     className="form-control"
                     dateFormat="Pp"
+                    showTimeSelect
+                    locale='es'
+                    timeCaption="Hora"
                 />
             </div>
 
@@ -75,11 +82,14 @@ export const CalendarModal = () => {
                 <label>Fecha y hora fin</label>
                 <br />
                 <DatePicker 
-                    minDate={ formValues.start}
+                    minDate={ formValues.start }
                     selected={ formValues.end }
                     onChange={ (event) => onDateChanged(event, 'end')}
                     className="form-control"
                     dateFormat="Pp"
+                    showTimeSelect
+                    locale='es'
+                    timeCaption="Hora"
                 />
             </div>
 
