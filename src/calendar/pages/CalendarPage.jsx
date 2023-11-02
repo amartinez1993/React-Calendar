b242f4203ef1dfffdef63dc91443d74ca52be342
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-11-02 17:49:56 +0100

    added modal with style

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
 import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns'
-import { CalendarEvent, NavBar } from "../"
+import { CalendarEvent, CalendarModal, NavBar } from "../"
 import { localizer, getMessagesES } from '../../helpers';
-import { useState } from 'react';
+
 
 
 const events = [{
@@ -72,6 +73,8 @@ export const CalendarPage = () => {
         onSelectEvent={ onSelect }
         onView={ onViewChanged }
       />
+
+      <CalendarModal />
 
     </>
   )
