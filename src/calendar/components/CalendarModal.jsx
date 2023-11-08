763de52e3d908600c30ab81533b9c7590dfa1d71
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-11-08 16:18:23 +0100

    get information from formevent

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { addHours } from 'date-fns';
+import { addHours, differenceInSeconds } from 'date-fns';
 
 import Modal from 'react-modal';
 import DatePicker, { registerLocale } from 'react-datepicker';
@@ -51,6 +51,25 @@ export const CalendarModal = () => {
         setIsOpen(false)
     }
 
+    const onSubmit = ( submit ) => {
+        event.preventDefault();
+
+        const difference = differenceInSeconds( formValues.end, formValues.start );
+        
+        if ( isNaN( difference ) || difference <= 0 ) {
+            console.log('Error en fechas');
+            return;
+        }
+
+        if ( formValues.title.length <= 0 ) return;
+        
+        console.log(formValues);
+
+        // TODO:
+        // cerrar modal
+        // remover errores de pantalla
+    }
+
   return (
     <Modal
         isOpen={ isOpen }
@@ -62,7 +81,7 @@ export const CalendarModal = () => {
     >
         <h1> Nuevo evento </h1>
         <hr />
-        <form className="container">
+        <form className="container" onSubmit={ onSubmit }>
 
             <div className="form-group mb-2">
                 <label>Fecha y hora inicio</label>
